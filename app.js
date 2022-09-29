bd5328c35f8e4941f121e51716ef1183ff6c8d64
shubhankarpatil <47353671+shubhankarpatil@users.noreply.github.com>
2022-09-29 12:02:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 app.use(express.static("files"));
-// app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
-
-// app.post("/", function(req, res){
-//   console.log(req.body.u);
-//   console.log(req.body.p);
-// });
 
 app.listen(3000, function(req, res){
   console.log("The server has started on port 3000");
